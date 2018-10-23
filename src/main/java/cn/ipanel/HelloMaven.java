5c92375466142a0575e6b67c612d6f712372e8aa
fangshenying <fangshenying@163.com>
2018-10-23 11:56:02 +0800

    提交Maven项目测试。

---
+++ src/main/java/cn/ipanel/HelloMaven.java
@@ -0,0 +1,11 @@
+package cn.ipanel;
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("第一个Maven项目诞生:"+" Hello Maven!!!");
+
+	}
+
+}
